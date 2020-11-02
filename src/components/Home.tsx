83c330290487c13ba6b3a72ed732869483ce2e87
hbashanae <bashana.e@radicalledger.com>
2020-11-03 01:19:32 +0530

    home page modified

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Typography, Link } from '@material-ui/core'
+import logo from '../images/did_siop_logo.png'
 
 export default function Home() {
 
     return (
-        <div style={{ textAlign: 'center' }}>
+        <div style={{ textAlign: 'center'}}>
+
+            <img width={250} src={logo} alt="logo" />
+
             <Typography>Essential tools to start using Self Issued OpenID Connect Provider
                 with Decentralised Identities (SIOP DID).
             
@@ -14,18 +18,19 @@ export default function Home() {
             <br />
             
             <Typography>
-                <b>Privacy Guaranteed</b> : When you are on this website,
+                <b>Privacy Guaranteed</b><br/>When you are on this website,
                 none of the info you see or generate does not leave your browser,
                 meaning it is perfectly secure and safe to use these services.
             </Typography >
 
-            <div style={{ display: "flex", minHeight: (window.innerHeight - 290), flexDirection: "column" }}></div>
-
+            <div style={{ display: "flex", minHeight: (window.innerHeight - 400), flexDirection: "column" }}></div>
+            
             <Typography>
                 <Link href="https://github.com/RadicalLedger/siop-tools-web">GitHub</Link>
                 <br />
                 <Link href="http://didsiop.org">www.didsiop.org</Link>
             </Typography>
+            
 
         </div>
     )
