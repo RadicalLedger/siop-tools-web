6382c187df1ae80c9cf6e45a42ed392bb5d7701e
hbashanae <bashana.e@radicalledger.com>
2020-11-03 01:17:52 +0530

    code clean

--- src/components/BTCAddress.tsx
+++ src/components/BTCAddress.tsx
@@ -6,15 +6,6 @@ import TextFieldNormal from './TextFieldNormal'
 export default function BTCAddress(props: { btcAddress: string; }) {
     return (
         <Grid item xs={12}>
-            {/* <TextField
-                id="standard-read-only-input"
-                label="Bitcoin Address"
-                fullWidth
-                value={props.btcAddress}
-                InputProps={{
-                    readOnly: true,
-                }}
-            /> */}
             <TextFieldNormal label="Bitcoin Address" value={props.btcAddress} />
         </Grid>
     )
